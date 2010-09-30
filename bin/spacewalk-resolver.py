5cc4721be5a0538965e3f34900567170e71ff11e
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-09-30 14:51:50 +0200

    the Plugin class got moved out

--- bin/spacewalk-resolver.py
+++ bin/spacewalk-resolver.py
@@ -13,106 +13,34 @@ from up2date_client import rhnChannel
 from up2date_client import up2dateAuth
 from up2date_client import up2dateErrors
 
-class Plugin:
-    """
-    The plugin class is offered by ZYpp as a helper
-    to handle communication between the plugin and
-    libzypp.
-
-    The protocol is similar to stomp, with each frame being:
-
-    COMMAND
-    headers
-
-    data
-    ^@
-
-    If libzypp calls the plugin with
-
-    INIT
-    Param: value
-    ^@
-
-    You will need to inherit from plugin:
-
-    class MyPlugin(Plugin)
-        def INIT(self, headers, body):
-            # ... do something
-            answer('COMMAND', {}, body)
-    """
+# for testing add the relative path to the load path
+if "spacewalk-resolver.py" in sys.argv[0]:
+    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../python/zypp'))
+else:
+    sys.path.insert(0, "/usr/share/zypp-plugin-spacewalk/python")
     
-    def __init__(self):
-        self.framestack = []
-        self.state = "START"
-        self.logger = logging.getLogger()
-        self.logger.setLevel(logging.DEBUG)
+from plugins import Plugin
 
-    class Frame(object):
-        def __init__(self, command):
-            self.command = command
-            self.headers = {}
-            self.body = ""
+class SpacewalkResolverPlugin(Plugin):
 
-    def answer(self, command, headers, body=""):
-        sys.stdout.write("%s\n" % command)
-        for k,v in headers.items():
-            sys.stdout.write("%s: %s\n" % (k,v))
-        sys.stdout.write("\n")            
-        sys.stdout.write(body)
-        sys.stdout.write("%s\n" % chr(0))
+    def RESOLVEURL(self, headers, body):
+
+        spacewalk_auth_headers = ['X-RHN-Server-Id', 
+                                  'X-RHN-Auth-User-Id',
+                                  'X-RHN-Auth',
+                                  'X-RHN-Auth-Server-Time',
+                                  'X-RHN-Auth-Expire-Offset']
         
-    def current_frame(self):
-        return self.framestack[len(self.framestack) - 1]
-
-    def main(self):
-        for line in iter(sys.stdin.readline, ''):
-            line = line.rstrip()
-            if line == "%s" % chr(0):
-                frame = self.framestack.pop()
-                if frame:
-                    method = getattr(self, frame.command)
-                    method(frame.headers, frame.body)
-                    self.state = "START"
-                    self.framestack = []
-                    continue
-            else:
-                if self.state == "START":
-                    if re.match("[\w]+", line):
-                        frame = self.Frame(line)
-                        self.framestack.append(frame)
-                        self.state = "HEADERS"
-                        continue
-                    if line == "":
-                      continue
-                    else:
-                        raise Exception("Expected command")
-                if self.state == "HEADERS":
-                    result = re.match("(\w+)\: (.+)", line)
-                    if line == "":
-                        self.state = "BODY"
-                        continue
-                    elif result:
-                        key = result.group(1)
-                        val = result.group(2)
-                        self.logger.debug("header: %s -> %s\n" % (key,val))
-                        self.current_frame().headers[key] = val
-                        continue
-                    else:
-                        raise Exception("Expect header or new line. Got %s" % line)
-                if self.state == "BODY":
-                    self.current_frame().body = self.current_frame().body + line + "\n"
-                    
-                
-class MediaPlugin(Plugin):
-
-    def INIT(self, headers, body):
         if not os.geteuid() == 0:
             # you can't access auth data if you are not root
             self.answer("ERROR", {}, "Can't access managed repositores without root access")
             return
+
+        
         if not headers['channel']:
-            self.answer("ERROR", {}, "Unknown channel")
+            self.answer("ERROR", {}, "Missing argument channel")
             return
+
         details = rhnChannel.getChannelDetails();
         for channel in details:
             if channel['label'] == headers['channel']:
@@ -120,21 +48,21 @@ class MediaPlugin(Plugin):
         if not self.channel:
             self.answer("ERROR", {}, "Can't retrieve information for channel %s" % headers['channel'])
             return
-        
+
+        self.auth_headers = {}
         try:
-            li = up2dateAuth.getLoginInfo()
-            self.auth_headers = li
+            login_info = up2dateAuth.getLoginInfo()
+            for k,v in login_info.items():
+                if k in spacewalk_auth_headers:
+                    self.auth_headers[k] = v
             #self.answer("META", li)
         except up2dateErrors.RhnServerException, e:
             self.answer("ERROR", {}, str(e))
+        
+        url = "%s/GET-REQ/%s" % (self.channel['url'], self.channel['label'])
+        self.answer("RESOLVEDURL", self.auth_headers, url)
 
-    def GETFILE(self, headers, body):
-        if not headers['path']:
-            self.answer("ERROR", {}, "Unknown path")
-            return
-        url = "%s/GET-REQ/%s%s" % (self.channel['url'], self.channel['label'], headers['path'])
-        print "Retrieve %s" % url
 
-plugin = MediaPlugin()
+plugin = SpacewalkResolverPlugin()
 plugin.main()
 
