e1367b733f882da4aea6996dcabca103dfd786b9
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-10-06 18:50:51 +0200

    - dont require a newline after the null character - flush stdout

--- python/zypp/plugins.py
+++ python/zypp/plugins.py
@@ -54,48 +54,56 @@ class Plugin:
     def answer(self, command, headers, body=""):
         sys.stdout.write("%s\n" % command)
         for k,v in headers.items():
-            sys.stdout.write("%s: %s\n" % (k,v))
+            sys.stdout.write("%s:%s\n" % (k,v))
         sys.stdout.write("\n")            
         sys.stdout.write(body)
-        sys.stdout.write("%s\n" % chr(0))
+        sys.stdout.write("%s" % chr(0))
+        sys.stdout.flush()
         
     def current_frame(self):
         return self.framestack[len(self.framestack) - 1]
 
+    def __collect_frame(self):
+        frame = self.framestack.pop()
+        if frame:
+            method = getattr(self, frame.command)
+            method(frame.headers, frame.body)
+            self.state = "START"
+            self.framestack = []
+
     def main(self):
         for line in iter(sys.stdin.readline, ''):
             line = line.rstrip()
-            if line == "%s" % chr(0):
-                frame = self.framestack.pop()
-                if frame:
-                    method = getattr(self, frame.command)
-                    method(frame.headers, frame.body)
-                    self.state = "START"
-                    self.framestack = []
+            if self.state == "START":
+                if re.match("[\w]+", line):
+                    frame = self.Frame(line)
+                    self.framestack.append(frame)
+                    self.state = "HEADERS"
                     continue
-            else:
-                if self.state == "START":
-                    if re.match("[\w]+", line):
-                        frame = self.Frame(line)
-                        self.framestack.append(frame)
-                        self.state = "HEADERS"
-                        continue
-                    if line == "":
-                      continue
-                    else:
-                        raise Exception("Expected command")
-                if self.state == "HEADERS":
-                    result = re.match("(\w+)\: (.+)", line)
-                    if line == "":
-                        self.state = "BODY"
-                        continue
-                    elif result:
-                        key = result.group(1)
-                        val = result.group(2)
-                        self.logger.debug("header: %s -> %s\n" % (key,val))
-                        self.current_frame().headers[key] = val
-                        continue
-                    else:
-                        raise Exception("Expect header or new line. Got %s" % line)
-                if self.state == "BODY":
-                    self.current_frame().body = self.current_frame().body + line + "\n"
+                if line == "":
+                    continue
+                else:
+                    raise Exception("Expected command")
+            if self.state == "HEADERS":
+                result = re.match("(\w+)\:\s*(.+)", line)
+                if line == "":
+                    self.state = "BODY"
+                    #print "wating for body"
+                    while 1:
+                        c = sys.stdin.read(1)
+                        if c == chr(0):
+                            #print "got it"
+                            break
+                        else:
+                            #print "no"
+                            self.current_frame().body = self.current_frame().body + c
+                    self.__collect_frame()
+                    continue
+
+                elif result:
+                    key = result.group(1)
+                    val = result.group(2)
+                    self.current_frame().headers[key] = val
+                    continue
+                else:
+                    raise Exception("Expect header or new line. Got %s" % line)
